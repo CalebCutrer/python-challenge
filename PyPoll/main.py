07eda0808cf6df5fa7010ced2aeaba1a5dbdbe68
CalebCutrer <cutr0003@outlook.com>
2024-04-18 18:25:04 -0500

    final main and text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,9 @@ import csv
 # Creating variables:
 Candidates_Set = set()
 Candidates_List = []
-Candidate1 = [] #Degette
-Candidate2 = [] #Stockham
-Candidate3 = [] #Doane
+Candidate1 = [] 
+Candidate2 = [] 
+Candidate3 = [] 
 Winner = []
 
 Total_Votes = 0
@@ -18,11 +18,30 @@ Percentage_Votes_Stockham = 0
 Percentage_Votes_DeGette = 0
 Percentage_Votes_Doane = 0
 
-
 # Create file path for data
 election_csv =os.path.join('Resources', 'election_data.csv')
+#-------------------------------------------------
 
-# Read in data, skip header row 
+# Read in data to determine candidates, skip header row 
+with open(election_csv) as csvfile: 
+    csv_reader = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(csv_reader)
+    
+#Getting a set of candidates who received votes 
+    for row in csv_reader:
+        Candidates_Set.add(row[2])
+
+#create a break in the code to stop it from reassigning each time code is ran?
+#Assigning Candidates
+    Candidates_List = list(Candidates_Set) #Complete list of candidates
+    #Candidate1 = Candidates_List[0]
+    #Candidate2 = Candidates_List[1]
+    #Candidate3 = Candidates_List[2]
+    print(Candidates_List)
+    
+#------------------------------------------------    
+
+# Read in data again, skip header row 
 with open(election_csv) as csvfile: 
     csv_reader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csv_reader)
@@ -52,36 +71,6 @@ with open(election_csv) as csvfile:
         else:
             Winner = "Charles Casper Stockham"
 
-    #print(f"Total votes DeGette: ", Total_Votes_DeGette)
-    #print(f"Total votes Doane: ", Total_Votes_Doane)
-    #print(f"Total votes Stockham: ", Total_Votes_Stockham)
-    #print(f"Percent votes DeGette: ", Percentage_Votes_DeGette)
-    #print(f"Percent votes Doane: ", Percentage_Votes_Doane)
-    #print(f"Percent votes Stockham: ", Percentage_Votes_Stockham)
-#
-#----------------------------
-# 
-
-# Read in data, skip header row 
-with open(election_csv) as csvfile: 
-    csv_reader = csv.reader(csvfile, delimiter = ',')
-    csv_header = next(csv_reader)
-    
-#Getting a set of candidates who received votes 
-    for row in csv_reader:
-        Candidates_Set.add(row[2])
-
-#create a break in the code to stop it from reassigning each time code is ran?
-#Assigning Candidates
-    Candidates_List = list(Candidates_Set)
-    Candidate1 = Candidates_List[0]
-    Candidate2 = Candidates_List[1]
-    Candidate3 = Candidates_List[2]
-    print(Candidates_List)
-    print(Candidate1)
-    print(Candidate2)
-    print(Candidate3)
-
 #
 #------------------------------------------------
 #
@@ -109,4 +98,4 @@ with open(outputpy, "w", newline = '') as textfile:
 
 
 # Sources: 
-# https://stackoverflow.com/questions/70551174/python-add-unique-values-from-a-csv-column-to-list (Used as guide to make list of candidates)
+# https://stackoverflow.com/questions/70551174/python-add-unique-values-from-a-csv-column-to-list (Used as guide to make list of candidates using a set)
