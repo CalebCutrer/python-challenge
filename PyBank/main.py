2b0194eb97e637719c2a16b62b4744d5c4670f85
CalebCutrer <cutr0003@outlook.com>
2024-04-17 19:52:10 -0500

    bad code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,25 +5,44 @@ import os
 import csv
 
 #Specify path to csv file in resources folder
-csvpath = os.path.join('PyBank', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 #Reading in file
 with open(csvpath) as csvfile: 
-        csvbudget = csv.reader(csvfile, delimiter = ',')
-        print(csvbudget)
-        # Did not work^^
-
+        csv_reader = csv.reader(csvfile, delimiter = ',')
+        print(csv_reader)
+        
         #Reading the header row (based on checking for a header directly)
-        csv_header = next(csvbudget)
+        csv_header = next(csv_reader)
         print(f"Header: {csv_header}")
 
         #Print subsequent rows
-        for row in csvbudget:
-                print(row)
+        for row in csv_reader:
+               print(row)
+   
+# Total Months:
+MonthCount = []
+with open(csvpath, 'r') as csv_file: 
+        csv_reader = csv.DictReader(csv_file, delimiter = ',')
+        for row in csv_reader:
+                MonthCount.append(row['Date'])
+                Count = len(MonthCount)
+print(Count)
+       
+# Total Profit/Losses
 
-# Writing function for the total number of months included in the dataset:
+netprofit = 0
 
-def Total_Months_Count(csvfile, column_index):
-        total_months = 0
+with open(csvpath, 'r') as csv_file: 
+        csv_reader = csv.reader(csv_file, delimiter = ',')
+        for row in csv_reader:
+                value = int(row[1])
+                netprofit += value
+        print(netprofit) 
 
 
+
+
+        
+
+#create path to file, another with open with a w for write, file.write 
