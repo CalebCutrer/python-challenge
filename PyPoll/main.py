459c41baf47ce15df443d0b36135be976fcbfb25
CalebCutrer <cutr0003@outlook.com>
2024-04-18 00:19:07 -0500

    Switched around

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -29,18 +29,23 @@ election_csv =os.path.join('Resources', 'election_data.csv')
 with open(election_csv) as csvfile: 
     csv_reader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csv_reader)
+
+# Total Votes:   
+    for row in csv_reader:
+        Total_Votes +=1
+    print(Total_Votes)
+
+# Read in data, skip header row 
+with open(election_csv) as csvfile: 
+    csv_reader = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(csv_reader)
     
-    #Getting a set of candidates who received votes 
-    #trying to get the values reassigned everytime code is ran 
-    i = 0 # doesnt work because it gets reset 
-    while i < 1:
-        for row in csv_reader:
-            Candidates_Set.add(row[2])
+#Getting a set of candidates who received votes 
+    for row in csv_reader:
+        Candidates_Set.add(row[2])
 
-        Candidates_List = list(Candidates_Set)
-        i = i +1
-
-    #Assigning Candidates
+#Assigning Candidates
+    Candidates_List = list(Candidates_Set)
     Candidate1 = Candidates_List[0]
     Candidate2 = Candidates_List[1]
     Candidate3 = Candidates_List[2]
@@ -48,10 +53,8 @@ with open(election_csv) as csvfile:
     print(Candidate1)
     print(Candidate2)
     print(Candidate3)
-    #Total number of votes cast
-    for row in csv_reader:
-        Total_Votes +=1
-    print(Total_Votes)
+
+    
 
 
 
