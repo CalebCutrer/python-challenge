641460258255e12bcb6eb171f65a352786c09f6d
CalebCutrer <cutr0003@outlook.com>
2024-04-17 22:06:55 -0500

    Finalized code and output

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,5 @@
+# I got assistance from Melissa from class on the formatting of my variables and for loops, we collaborated on this assignment over zoom. 
+
 #Import os module to create file paths
 import os
 
@@ -12,37 +14,95 @@ with open(csvpath) as csvfile:
         csv_reader = csv.reader(csvfile, delimiter = ',')
         print(csv_reader)
         
-        #Reading the header row (based on checking for a header directly)
+        #Checking for header
         csv_header = next(csv_reader)
         print(f"Header: {csv_header}")
 
-        #Print subsequent rows
+        #Looking at subsequent rows
         for row in csv_reader:
                print(row)
-   
-# Total Months:
-MonthCount = []
-with open(csvpath, 'r') as csv_file: 
-        csv_reader = csv.DictReader(csv_file, delimiter = ',')
+
+# Creating the variables we need:
+TotalMonths = 0
+NetTotalProfit = 0
+LastMonthProfit = 0
+ThisMonthProfit = 0
+TotalProfitChange = 0
+AverageProfitChange = 0
+
+# The following variables were the ones Melissa helped me format correctly:
+GreatestIncrease = float('-inf')
+GreatestDecrease = float('inf')
+MonthIncrease = None 
+MonthDecrease = None
+FirstMonth = True 
+
+#Reading in file again, making sure to skip header:
+with open(csvpath) as csvfile: 
+        csv_reader = csv.reader(csvfile, delimiter = ',')
+        print(csv_reader)
+        csv_header = next(csv_reader)
+
+ #Finding the total number of months, as well as the total profits/losses over this period
         for row in csv_reader:
-                MonthCount.append(row['Date'])
-                Count = len(MonthCount)
-print(Count)
-       
-# Total Profit/Losses
+                LastMonthProfit = ThisMonthProfit
+                TotalMonths += 1
+                ThisMonthProfit = float(row[1])
+                NetTotalProfit += ThisMonthProfit
+                
+                Date = row[0] #will be used later to determine the MonthIncrease and MonthDecrease values
+                ProfitChange = ThisMonthProfit - LastMonthProfit #Tracks monthly profit fluctuations
+                
+                #Skipping first row because there is no change yet given there is no data on the previous month
+                if not FirstMonth: 
+                        TotalProfitChange += ProfitChange
+                else:
+                        FirstMonth = False
 
-netprofit = 0
+                #Greatest profit increase and corresponding month
+                if ProfitChange > GreatestIncrease:
+                        GreatestIncrease = ProfitChange
+                        MonthIncrease = Date
+                
+                Date = row[0] #Reset date value to begin at the first row/month once we find greatest increase
+                
+                #Greatest profit decrease and corresponding month
+                if ProfitChange < GreatestDecrease:
+                        GreatestDecrease = ProfitChange
+                        MonthDecrease = Date
+        
+        #Finding average profit change over time period using  
+        AverageProfitChange = TotalProfitChange/(TotalMonths-1) 
+                
+# Creating output summary and writing it into a text file
+output = os.path.join('analysis', 'BankAnalysis') #Creating path to folder
+with open(output, "w", newline = '') as textfile:
+        writer = csv.writer(textfile)
 
-with open(csvpath, 'r') as csv_file: 
-        csv_reader = csv.reader(csv_file, delimiter = ',')
-        for row in csv_reader:
-                value = int(row[1])
-                netprofit += value
-        print(netprofit) 
-
-
-
-
+        print("\nFinancial Analysis", file = textfile)
+        print("\n--------------------------------------------\n", file = textfile)
+        print(f"Total Months: {TotalMonths}",file = textfile)
+        print(f"\nTotal: ${round(NetTotalProfit)}", file = textfile) 
+        print(f"\nAverage Change: ${round(AverageProfitChange, ndigits=2)}", file = textfile)
+        print(f"\nGreatest Increase in Profits: {MonthIncrease} (${round(GreatestIncrease)})", file = textfile)
+        print(f"\nGreatest Decrease in Profits: {MonthDecrease} (${round(GreatestDecrease)})\n", file = textfile)
         
 
-#create path to file, another with open with a w for write, file.write 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
